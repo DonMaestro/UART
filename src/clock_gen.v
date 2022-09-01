d8343d46f1ca2e3ddd267ed88d8a90f2cc825382
DonMaestro <vitalik16200@gmail.com>
2022-09-01 12:31:27 +0300

    feat(src/clock_gen.v): add synchronous reset

--- src/clock_gen.v
+++ src/clock_gen.v
@@ -4,22 +4,27 @@ module clock_gen #(
 	parameter SIZE = 434
 ) (
 	output o_clk,
+	input  i_srst,
 	input  i_nrst,
 	input  i_clk
 );
 
 reg [WIDTH-1:0] cnt;
 
-assign o_clk = cnt[WIDTH-1];
+assign o_clk = cnt == 0;
 
 always @(posedge i_clk, negedge i_nrst) begin
 	if (!i_nrst)
 		cnt <= {WIDTH{1'b0}};
 	else begin
-		if (SIZE - 1 == cnt)
-			cnt <= {WIDTH{1'b0}};
-		else
-			cnt <= cnt + 1;
+		if (i_srst)
+			cnt <= 4;
+		else begin
+			if (SIZE - 1 == cnt)
+				cnt <= {WIDTH{1'b0}};
+			else
+				cnt <= cnt + 1;
+		end
 	end
 end
 
