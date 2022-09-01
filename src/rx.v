7d51a486633aee323c9416173506058ae97d7d8b
DonMaestro <vitalik16200@gmail.com>
2022-09-01 12:30:14 +0300

    fix(src/rx.v)

--- src/rx.v
+++ src/rx.v
@@ -7,43 +7,56 @@ module rx #(
 	// inside in the chip
 	output reg              o_rdy,
 	output [WIDTH_DATA-1:0] o_data,
+	output                  o_srst_clk,
 	input                   i_re,
 	input                   i_nrst,
 	input                   i_clk,
 	input                   clk_rx
 );
 
-localparam NB_STATE = 1 + WIDTH_DATA + NB_STOP;
+localparam STATE_DATA_FIRST = 0;
+localparam STATE_DATA_LAST  = WIDTH_DATA - 1;
+localparam STATE_STOP_FIRST = WIDTH_DATA;
+localparam STATE_STOP_LAST  = WIDTH_DATA + NB_STOP - 1;
+localparam STATE_IDLE       = STATE_STOP_LAST + 1;
+localparam STATE_START      = STATE_IDLE + 1;
 
-reg [1:0] fr_det;
-reg [1:0] start_det;
-wire pe_ev = fr_det[1] && ~fr_det[0];
-wire start_ev = ~start_det[1] && start_det[0];
+reg [1:0] detr_clk_front;
+reg [1:0] detr_start;
+reg ev_start;
 
 reg [WIDTH_DATA-1:0] sipo;
 
 reg [3:0] state;
 
-wire c_start = 4'b0 == state && ~o_rdy && pe_ev;
-wire c_sipo = pe_ev;
+wire ev_pe = detr_clk_front[1] && ~detr_clk_front[0];
+wire en_sipo = ev_pe && STATE_DATA_FIRST <= state && STATE_DATA_LAST >= state;
 
 assign o_data = sipo;
+assign o_srst_clk = ev_start;
+
+always @(*) begin
+	if (STATE_STOP_LAST == state || STATE_IDLE == state)
+		ev_start = ~detr_start[1] && detr_start[0];
+	else
+		ev_start = 1'b0;
+end
 
 // front detector
 always @(posedge i_clk, negedge i_nrst) begin
 	if (!i_nrst)
-		fr_det <= 2'b0;
+		detr_clk_front <= 2'b0;
 	else begin
-		fr_det <= {clk_rx, fr_det[1]};
+		detr_clk_front <= {clk_rx, detr_clk_front[1]};
 	end
 end
 
 // start detector
 always @(posedge i_clk, negedge i_nrst) begin
 	if (!i_nrst)
-		start_det <= 2'b0;
+		detr_start <= 2'b0;
 	else begin
-		start_det <= {i_buf, start_det[1]};
+		detr_start <= {i_buf, detr_start[1]};
 	end
 end
 
@@ -55,7 +68,7 @@ always @(posedge i_clk, negedge i_nrst) begin
 		if (i_re)
 			o_rdy <= 1'b0;
 
-		if (WIDTH_DATA + 1 == state && pe_ev)
+		if (STATE_DATA_LAST == state && ev_pe)
 			o_rdy <= 1'b1;
 	end
 end
@@ -65,7 +78,7 @@ always @(posedge i_clk, negedge i_nrst) begin
 	if (!i_nrst)
 		sipo <= {WIDTH_DATA{1'b1}};
 	else begin
-		if (c_sipo)
+		if (en_sipo)
 			sipo <= {i_buf, sipo[WIDTH_DATA-1:1]};
 	end
 end
@@ -77,15 +90,15 @@ end
 // state counter
 always @(posedge i_clk, negedge i_nrst) begin
 	if (!i_nrst)
-		state <= 4'b0;
+		state <= STATE_IDLE;
 	else begin
-		if (NB_STATE - 1 == state && pe_ev)
-			state <= 4'b0;
-		else if ((start_ev && !state) || (state && pe_ev))
-			state <= state + 4'b1;
+		case (state)
+		STATE_IDLE: state <= ev_start ? STATE_START : state;
+		STATE_START: state <= ev_pe ? STATE_DATA_FIRST : state;
+		default: state <= ev_pe ? state + 1 : state;
+		endcase
 	end
 end
-
 
 endmodule
 
